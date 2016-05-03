be836b29e6bafa4eb063b48240557781ef97b802
Marco Zille <marco.zille@gmail.com>
2016-05-03 23:26:52 +0200

    Aggiunta funzione per aggiungere un attributo ad una procedura

--- oberonTools.hs
+++ oberonTools.hs
@@ -21,22 +21,41 @@ data Attribute = Attribute {	attributeName :: String,
 								integerArrayValue :: [Integer],
 								charArrayValue :: [Char],
 								isConstant :: Bool,
-								isPassedByReference :: Bool } deriving (Show)
+								isPassedByReference :: Bool } deriving (Show)								
 
 data Procedure = Procedure { 	procedureName :: String,
 								attributes :: [Attribute],
 								procedureProcedures :: [Procedure] } deriving (Show)
 
-data ProcedureStack = ProcedureStack{
-	levels :: [Procedure]
-}deriving(Show)
-
-pushProcedureToStack
-  :: Procedure -> ProcedureStack -> ProcedureStack
-pushProcedureToStack p (ProcedureStack{levels = l}) = ProcedureStack{levels=(p:l)}
-
-lookProcedureToStack :: ProcedureStack -> Maybe Procedure
-lookProcedureToStack (ProcedureStack{levels = (x:xs)}) = Just x
-lookProcedureToStack (ProcedureStack{levels = []}) = Nothing
+data ProcedureStack = ProcedureStack { levels :: [Procedure] }deriving(Show)
 
 data Program = Program { programProcedures :: [Procedure] } deriving (Show)
+
+defaultAttribute = Attribute {	attributeName = "",
+								attributeType = Integer,
+								stringValue = "",
+								floatValue = 0.0,
+								integerValue = 0,
+								charValue = ' ',
+								stringArrayValue = [],
+								floatArrayValue = [],
+								integerArrayValue = [],
+								charArrayValue = [],
+								isConstant = False,
+								isPassedByReference = False }
+
+defaultProcedure = Procedure { 	procedureName = "",
+								attributes = [],
+								procedureProcedures = [] }
+
+pushProcedureToStack :: Procedure -> ProcedureStack -> ProcedureStack
+pushProcedureToStack p (ProcedureStack { levels = l } ) = ProcedureStack { levels = (p : l) }
+
+lookProcedureToStack :: ProcedureStack -> Maybe Procedure
+lookProcedureToStack (ProcedureStack { levels = (x:xs) }) = Just x
+lookProcedureToStack (ProcedureStack { levels = [] }) = Nothing
+
+addAttributeToProcedure :: Procedure -> Attribute -> Procedure
+addAttributeToProcedure proc att = Procedure { 	procedureName = (procedureName proc),
+												attributes = (attributes proc) ++ [att],
+												procedureProcedures = (procedureProcedures proc) }
