d606045930dc308e26fbe05f9ffcb8c11b7a62f0
Minskal <firelion94@hotmail.it>
2016-05-04 11:25:41 +0200

    writeInt readInt insieme

---
+++ writeInt.hs
@@ -0,0 +1,31 @@
+
+--writeInt :: Int -> Int
+ 
+ {-
+ 
+
+
+x <- getLine
+
+let y = read x :: Int
+
+return y
+
+
+
+main = do
+    x <- readInt
+    print x
+
+    
+    -}    
+
+    
+--readInt :: Integer -> Integer
+
+    
+main = do
+    x <- getLine
+    let y = read x :: Integer
+    print y
+    
