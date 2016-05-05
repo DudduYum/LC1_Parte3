0a6d24c0be947556263255642fa6c0a864519b69
Minskal <firelion94@hotmail.it>
2016-05-05 22:49:09 +0200

    Update Pretty-print

--- test/Pretty-printer.hs
+++ test/Pretty-printer.hs
@@ -5,7 +5,7 @@
 module Pretty-printer where
 
 import Text.PrettyPrint
---import Text.PrettyPrint.HughesPJClass
+--import Text.PrettyPrint.HughesPJClass ( se necessaria )
 
 -- Qui bisognerà implementare la struttura dati utilizzata dal parser
 
@@ -13,7 +13,7 @@ data Program a =        Procedure ( Program a )
                |        Attribute a deriving (Show)
                
                
--- Si procede poi a definire come sarà l'indentazione per casi
+-- Si procede poi a definire come sarà l'indentazione per casi ( ricorsivamente )
                
 instance Pretty a => Pretty ( Program a ) where
     pPrint ( Attribute a ) = text "VAR: " <> pPrint a
@@ -21,39 +21,48 @@ instance Pretty a => Pretty ( Program a ) where
                                     , nest 1 ( pPrint p )]
                                     
 
-{- con Procedure e Attribute che possono essere uno tra i seguenti:
- "INTEGER"                 
- "REAL"                    
- "BOOLEAN"                 
- "CHAR"                    
- "SET"                     
- "ARRAY"                   
- "OF"                      
- "POINTER TO"              
- "PROCEDURE"               
- "BEGIN"                  
- "END"                     
- "VAR"                    
- "CONST"                   
- "TRUE"                    
- "FALSE"                   
- "IF"                      
- "ELSIF"                   
- "ELSE"                    
- "THEN"                    
- "CASE"                    
- "WHILE"                   
- "DO"                      
- "REPEAT"                  
- "UNTIL"                   
- "LOOP"                    
- "EXIT"                    
- "RETURN"                 
- "BREAK"                   
- "CONTINUE"                
+{- con Procedure e Attribute che possono essere uno tra i seguenti:       ( A > attributes, P > procedures )
+ "INTEGER"        A         
+ "REAL"           A         
+ "BOOLEAN"        A         
+ "CHAR"           A         
+ "SET"            P         
+ "ARRAY"          A         
+ "POINTER TO"     A         
+ "PROCEDURE"      P         
+ "BEGIN"          P        
+ "END"            P         
+ "VAR"            A        
+ "CONST"          A        
+ "IF"             P         
+ "ELSIF"          P         
+ "ELSE"           P         
+ "THEN"           P         
+ "CASE"           P         
+ "WHILE"          P         
+ "DO"             P         
+ "REPEAT"         P         
+ "UNTIL"          P         
+ "LOOP"           P         
+ "EXIT"           P         
+ "RETURN"         P        
+ "BREAK"          P         
+ "CONTINUE"       P         
 -}
 
 -- Infine si fornisce pure qualche esempio ai fini della comprensione
 
+{- Esempio di indentazione:
 
+PROCEDURE p1;
+	    PROCEDURE p12;
+	 	       PROCEDURE p123;
+		        END p123
+     END p12;
+	    PROCEDURE p22;
+	         PROCEDURE p1234;
+	         END p1234
+	    END p22
+END p1
 
+-}
